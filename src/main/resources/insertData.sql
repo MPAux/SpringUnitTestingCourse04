0bbcebb210e800fc236f82967eb8e8de6cbf9b2a
MPAux <mikepoweraux@gmail.com>
2024-03-20 16:14:46 +0100

    Hasta el vídeo 75. Se terminan las pruebas con integracion de base de datos (embebida en memoria)

---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,5 @@
+//insert into student(id, firstname, lastname, email_address) values (11, 'Student', 'One', 'student.one@luv2code_school.com');
+insert into student(id, firstname, lastname, email_address) values (22, 'Student', 'Two', 'student.two@luv2code_school.com');
+insert into student(id, firstname, lastname, email_address) values (33, 'Student', 'Three', 'student.three@luv2code_school.com');
+insert into student(id, firstname, lastname, email_address) values (44, 'Student', 'Four', 'student.four@luv2code_school.com');
+insert into student(id, firstname, lastname, email_address) values (55, 'Student', 'Five', 'student.five@luv2code_school.com');
